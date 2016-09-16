0228e359d292ec1202c69af45b4a8adf3d2359d0
sheafk <sheafureyking@gmail.com>
2016-09-16 13:57:09 -0400

    Variables for each number total

--- LuckTracker/ViewController.swift
+++ LuckTracker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTotalNumberOfRolls()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +22,28 @@ class ViewController: UIViewController {
     }
 
 
+    var dice1Total = Int()
+    var dice2Total = Int()
+    var dice3Total = Int()
+    var dice4Total = Int()
+    var dice5Total = Int()
+    var dice6Total = Int()
+    var dice7Total = Int()
+    var dice8Total = Int()
+    var dice9Total = Int()
+    var dice10Total = Int()
+    var dice11Total = Int()
+    var dice12Total = Int()
+    
+    @IBOutlet weak var totalNumOfRolls: UILabel!
+    
+    func setTotalNumberOfRolls(){
+        
+        let total = dice1Total + dice2Total + dice3Total + dice4Total + dice5Total + dice6Total + dice7Total + dice8Total + dice9Total + dice10Total + dice11Total + dice12Total
+       self.totalNumOfRolls.text! = String(total)
+        print("set total numbers")
+    }
+    
+    
 }
 
