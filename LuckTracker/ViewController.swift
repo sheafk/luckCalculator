ad0a63b26489b859d48e0eb00d65e171f5a5c37c
sheafk <sheafureyking@gmail.com>
2016-09-16 14:52:00 -0400

    UILabels for Averages and Totals Added to Stackview

--- LuckTracker/ViewController.swift
+++ LuckTracker/ViewController.swift
@@ -21,8 +21,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-    var dice1Total = Int()
     var dice2Total = Int()
     var dice3Total = Int()
     var dice4Total = Int()
@@ -37,10 +35,70 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var totalNumOfRolls: UILabel!
     
+    func addOneRoll(dice: Int) -> (Int) {
+        
+        var dicePlusOne = dice + 1
+        
+        return dicePlusOne
+        
+    }
+    
+    
+    @IBAction func dice2ButtonTapped(sender: AnyObject) {
+        
+        dice2Total = dice2Total + 1
+        print("Someone rolled a 1. Total number of 1's is \(dice2Total)")
+    }
+    
+    @IBAction func dice3ButtonTapped(sender: AnyObject) {
+        dice3Total = dice3Total + 1
+        print("Someone rolled a 3. Total number of 3's is \(dice3Total)")
+    }
+    
+    @IBAction func dice4ButtonTapped(sender: AnyObject) {
+        
+        dice4Total = dice4Total + 1
+        print("Someone rolled a 4. Total number of 4's is \(dice4Total)")
+    }
+    
+    @IBAction func dice5ButtonTapped(sender: AnyObject) {
+        
+        dice5Total = dice5Total + 1
+        print("Someone rolled a 5. Total number of 5's is \(dice5Total)")
+    }
+    
+    @IBAction func dice6ButtonTapped(sender: AnyObject) {
+        
+        dice6Total = dice6Total + 1
+        print("Someone rolled a 6. Total number of 6's is \(dice6Total)")
+    }
+    
+    @IBAction func dice7ButtonTapped(sender: AnyObject) {
+        
+        dice7Total = dice7Total + 1
+        print("Someone rolled a 7. Total number of 7's is \(dice7Total)")
+    }
+    
+    @IBAction func dice8ButtonTapped(sender: AnyObject) {
+        
+        dice8Total = dice8Total + 1
+        print("Someone rolled a 8. Total number of 8's is \(dice8Total)")
+    }
+    
+    @IBAction func dice9ButtonTapped(sender: AnyObject) {
+        
+        addOneRoll(dice9Total)
+        print("Someone rolled a 9. Total number of 9's is \(dice9Total)")
+    }
+    
+    
+    
+    
+    
     func setTotalNumberOfRolls(){
         
-        let total = dice1Total + dice2Total + dice3Total + dice4Total + dice5Total + dice6Total + dice7Total + dice8Total + dice9Total + dice10Total + dice11Total + dice12Total
-       self.totalNumOfRolls.text! = String(total)
+        let total = dice2Total + dice3Total + dice4Total + dice5Total + dice6Total + dice7Total + dice8Total + dice9Total + dice10Total + dice11Total + dice12Total
+
         print("set total numbers")
     }
     
