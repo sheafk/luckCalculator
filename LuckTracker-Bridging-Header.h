a44841858ea023bc35c20433c8a1bfb0bf1bbc94
sheafk <sheafureyking@gmail.com>
2016-09-23 12:16:41 -0400

    Imported BFPaperButton to project and added BridgingHeader file to use the Objective-C lib

---
+++ LuckTracker-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LuckTracker-Bridging-Header.h
+//  LuckTracker
+//
+//  Created by Shea Furey-King on 9/23/16.
+//  Copyright © 2016 Shea Furey-King. All rights reserved.
+//
+
+#ifndef LuckTracker_Bridging_Header_h
+#define LuckTracker_Bridging_Header_h
+
+#import <BFPaperButton/BFPaperButton.h>
+
+#endif /* LuckTracker_Bridging_Header_h */
---
+++ LuckTracker/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  LuckTracker
+//
+//  Created by Shea Furey-King on 9/23/16.
+//  Copyright © 2016 Shea Furey-King. All rights reserved.
+//
+
+import UIKit
+
+class Game: NSObject {
+
+    
+
+}
